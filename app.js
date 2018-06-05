2b3d4a7ba5672cf765584da7c07f196afd912457
maciejberka <maciejberka@gmail.com>
2018-06-05 12:43:36 +0200

    Add array of cat-objects

--- app.js
+++ app.js
@@ -2,6 +2,38 @@
 
 const model = {
   
+  cats: [
+    {
+      name: 'Puszek',
+      clicks: 0,
+      image: 'img/cat_1.jpg'
+    },
+    
+    {
+      name: 'Marta',
+      clicks: 0,
+      image: 'img/cat_2.jpg'
+    },
+    
+    {
+      name: 'Filemon',
+      clicks: 0,
+      image: 'img/cat_3.jpg'
+    },
+    
+    {
+      name: 'Kracy',
+      clicks: 0,
+      image: 'img/cat_4.jpg'
+    },
+    
+    {
+      name: 'Janusz',
+      clicks: 0,
+      image: 'img/cat_5.jpg'
+    } 
+  ]
+  
 }
 
 //OCTOPUS object contain methods which are using to communicate MODEL object and VIEW objects
