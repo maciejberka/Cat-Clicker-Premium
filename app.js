0619bc4ffdd61647a4a1b5abcac4f7d303cdc3e3
maciejberka <maciejberka@gmail.com>
2018-06-05 12:34:56 +0200

    Add JS structure for Model-View-Octopus pattern

---
+++ app.js
@@ -0,0 +1,21 @@
+//MODEL object contain data using by app
+
+const model = {
+  
+}
+
+//OCTOPUS object contain methods which are using to communicate MODEL object and VIEW objects
+
+const octopus = {
+  
+}
+
+//VIEW objects contain methods using to render view for a user
+
+const listView = {
+  
+}
+
+const areaView = {
+  
+}
